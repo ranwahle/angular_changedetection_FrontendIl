414fc22b110e9a0f6a5a66bfeacacbac1f1c4fb6
ranwahle <ran.wahle@gmail.com>
2016-09-27 08:15:27 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodoListComponentComponent } from './todo-list-component/todo-list-component.component';
+import { TaskComponentComponent } from './task-component/task-component.component';
+import { TaskOwnerComponentComponent } from './task-owner-component/task-owner-component.component';
+import { EditTaskComponentComponent } from './edit-task-component/edit-task-component.component';
+import { EditUserComponentComponent } from './edit-user-component/edit-user-component.component';
+import { DropdownComponentComponent } from './dropdown-component/dropdown-component.component';
+import { DropdownFilterPipe } from './dropdown-filter.pipe';
+import { ShowTopPipe } from './show-top.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponentComponent,
+    TaskComponentComponent,
+    TaskOwnerComponentComponent,
+    EditTaskComponentComponent,
+    EditUserComponentComponent,
+    DropdownComponentComponent,
+
+    DropdownFilterPipe,
+
+    ShowTopPipe
   ],
   imports: [
     BrowserModule,
